b69eda62e2ac201bc01754671427d186d483b4d8
MrSkorohod <mr.vl.skorohod@gmail.com>
2021-05-22 11:42:38 +0300

    Add firs Homework

---
+++ ishop/script.js
@@ -0,0 +1,27 @@
+var tableShop = React.createClass({
+    displayName: "tableShop",
+
+    getDefaultProps: function() {
+        return {name: "Undefined shop"}
+    },
+
+    render: function() {
+
+        var productsCode = [];
+        for (var i = 0; i < this.props.products.length; i++) {
+            var product = this.props.products[i];
+            var productCode =
+                React.DOM.div({key: product.code},
+                    React.DOM.div({className: "headProduct"}, product.name),
+                    React.DOM.div({className: "prise"}, `Prise: ${product.prise}`),
+                    React.DOM.img({src: `${product.image}`, className: "image"}, null),
+                    React.DOM.div({className: "inStock"}, `In Stock: ${product.in_stok}`),
+                );
+            productsCode.push(productCode);
+        }
+
+        return React.DOM.div({className: "mainTable"},
+            React.DOM.div({className: "header"}, this.props.name),
+            React.DOM.div({className: "productTable"}, productsCode));
+    }
+});
