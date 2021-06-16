1236f5a0af95b67b9e04cec5b1602a1e7ed0d436
MrSkorohod <mr.vl.skorohod@gmail.com>
2021-06-16 10:16:12 +0300

    Add new code

--- ishop2/Ishop.js
+++ ishop2/Ishop.js
@@ -1,15 +1,32 @@
 let Ishop = React.createClass({
     defaultName: "TableShop",
 
+    getInitialState: function(){
+        return{
+            selectedItemId: "",
+            className: "checkedTd"
+        }
+    },
+
+    checked: function(event){
+        this.setState({selectedItemId: event.target.id});
+        if(event.target.id === this.state.selectedItemId){
+            event.target.className = this.state.className;
+        }
+
+        console.log(this.state);
+    },
+
      render: function () {
-        return React.DOM.table(null,
+        return React.DOM.table({className: "table"},
             React.DOM.thead(null,
                 React.DOM.tr(null,
-                    React.DOM.th(null, `Name`),
-                    React.DOM.th(null, `Prise`),
-                    React.DOM.th(null, `URL`),
-                    React.DOM.th(null, `In stok`))
+                    React.DOM.th({className: "th"}, `Name`),
+                    React.DOM.th({className: "th"}, `Prise`),
+                    React.DOM.th({className: "th"}, `URL`),
+                    React.DOM.th({className: "th"}, `In stok`),
+                    React.DOM.th({className: "th"}, `Control`))
                 ),
-            React.createElement(Product, {products: this.props.products}))
+            React.createElement(Product, {products: this.props.products, clChecked: this.checked}))
      },
 });
--- ishop2/Product.js
+++ ishop2/Product.js
@@ -1,20 +1,40 @@
 "use strict";
 let Product = React.createClass({
     displayName: "Product",
+    // getInitialState: function(){
+    //     return{
+    //         selectedItemId: "",
+    //         className: "checkedTd"
+    //     }
+    // },
+
+    // checked: function(event){
+    //     this.setState({selectedItemId: event.target.id});
+    //     if(event.target.id === this.state.selectedItemId){
+    //         event.target.className = this.state.className;
+    //     }
+    //
+    //     console.log(this.state);
+    // },
+
 
     render: function() {
+        const button = React.DOM.button(null, `Delete`);
          let arrProducts = [];
          for(let i = 0; i < this.props.products.length; i++){
              let products = this.props.products[i];
              let productCode =
-                 React.DOM.tr({key: products.code},
-                     React.DOM.td(null, products.name),
-                     React.DOM.td(null, products.price),
-                     React.DOM.td(null, products.URL),
-                     React.DOM.td(null, products.in_stok)
+                 React.DOM.tr({key: products.code, onClick: this.props.clChecked, id: products.id},
+                     React.DOM.td({className: "td"}, products.name),
+                     React.DOM.td({className: "td"}, products.prise),
+                     React.DOM.td({className: "td"}, products.URL),
+                     React.DOM.td({className: "td"}, products.in_stok),
+                     React.DOM.td({className: "td"}, button),
                  );
              arrProducts.push(productCode);
          }
-         return React.DOM.tbody(arrProducts);
+
+
+         return React.DOM.tbody(null,arrProducts);
     },
 });
